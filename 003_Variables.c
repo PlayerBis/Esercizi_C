70b0d58d0476658eba03b1cc8dc9b04d74c577c3
Emanuel Bisicchia <emanuel.bisicchia99@gmail.com>
2022-10-21 14:00:25 +0200

    feat: data types

--- 003_Variables.c
+++ 003_Variables.c
@@ -1,9 +1,19 @@
 /**
- * Stampa tutti i tipi di dato, con la loro corrispettiva dimensione
+ * Stampa i tipi di dato principali con i loro specificatori.
  */
 
 #include <stdio.h>
 
 int main() {
+    int età = 34;             //  Numero intero
+    float altezza = 1.86;     //  Numero decimale
+    char voto = 'A';          //  Singolo carattere
+    char name[] = "Emanuel";  //  Array di caratteri
+
+    printf("Ciao, %s.\n", name);                         //  Stringhe
+    printf("Tu hai %d anni.\n", età);                    //  Interi
+    printf("La tua altezza è di %f cm.\n", altezza);     //  Float
+    printf("Il voto con cui sei uscito è: %c\n", voto);  //  Carattere
+
     return 0;
 }
---
+++ 004_DataTypes.c
@@ -0,0 +1,44 @@
+/**
+ * Tutti i tipi di dato
+ */
+
+#include <stdbool.h>  // Per il tipo di dato bool
+#include <stdio.h>
+
+int main() {
+    char a = 'C';          //  carattere singolo    %c
+    char b[] = "Emanuel";  //  array di caratteri   %s
+
+    float c = 3.141592;            // 4 bytes    %f
+    double d = 3.141592653589793;  // 8 bytes    %lf
+
+    bool e = true;  // 1 bytes  %d
+
+    char f = 100;           //  1 bytes %d or %c
+    unsigned char g = 255;  //  1 bytes %d or %c
+
+    short int h = 32767;           //  2 bytes %d
+    unsigned short int i = 65535;  //  2 bytes %d
+
+    int j = 2147483647;            //  4 bytes %d
+    unsigned int k = 4294967295L;  //  4 bytes %d
+
+    long long int l = 9223372036854775807;             //  8 bytes %lld
+    unsigned long long int m = 18446744073709551615U;  //  8 bytes %llu
+
+    printf("\nchar a: %c\n", a);
+    printf("char b[]: %s\n\n", b);
+    printf("float c: %0.2f\n", c);  // 0.2 indica le cifre dopo la virgola
+    printf("double d: %lf\n\n", d);
+    printf("bool e: %d\n\n", e);
+    printf("char f: %d\n", f);
+    printf("unsigned char g: %d\n\n", g);
+    printf("short int h: %d\n", h);
+    printf("unsigned short int i: %d\n\n", i);
+    printf("int j: %d\n", j);
+    printf("unsigned int k: %u\n\n", k);
+    printf("long long int l: %lld\n", l);
+    printf("unsigned long long int m: %llu\n\n", m);
+
+    return 0;
+}
