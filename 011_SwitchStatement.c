c156ccb541c5e2a584e67b2cdffe7a5263b4c1d3
Emanuel Bisicchia <emanuel.bisicchia99@gmail.com>
2022-10-23 09:43:15 +0200

    feat: new file

---
+++ 011_SwitchStatement.c
@@ -0,0 +1,40 @@
+/**
+ *  Scrivi un programma che ti dia un feedback
+ *  in base al voto che hai preso.
+ */
+
+#include <stdio.h>
+
+#define EXIT_SUCCESS 0
+
+int main() {
+    char grade;
+
+    printf("\nScrivi il tuo voto in lettera [A,F]: ");
+    scanf("%c", &grade);
+
+    switch (grade) {
+        case 'A':
+            printf("\nPerfetto!\n\n");
+            break;
+        case 'B':
+            printf("\nOttimo!\n\n");
+            break;
+        case 'C':
+            printf("\nPotevi fare meglio.\n\n");
+            break;
+        case 'D':
+            printf("\nSei andato male.\n\n");
+            break;
+        case 'E':
+            printf("\nSei andato molto male.\n\n");
+            break;
+        case 'F':
+            printf("\nNon hai studiato nulla. \n\n");
+            break;
+        default:
+            printf("\nPerfavore inserisci un voto valido.\n\n");
+    }
+
+    return EXIT_SUCCESS;
+}
