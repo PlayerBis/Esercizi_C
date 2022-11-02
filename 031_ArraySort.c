a79acedd69cfc5cf1c56734491c36f59cfd6fbd1
Emanuel Bisicchia <emanuel.bisicchia99@gmail.com>
2022-11-02 11:19:23 +0100

    fix: num[] --> array[]

--- 031_ArraySort.c
+++ 031_ArraySort.c
@@ -7,28 +7,28 @@
 #define EXIT_SUCCESS 0
 
 //  Prototipo
-void sort(int[], int);
-void printArray(int[], int);
+void sort(char[], int);
+void printArray(char[], int);
 
 int main() {
-    int num[] = {3, 6, 12, 48, 34, 2, 45, 1293, 356, 1, -34, -20};
-    int numSize = sizeof(num) / sizeof(num[0]);
+    char array[] = {'c', 'd', 'a', 'b', 'z', 'g', 'o', 'n', 'q', 'u', 'm'};
+    int arraySize = sizeof(array) / sizeof(array[0]);
 
     printf("\n");
-    sort(num, numSize);
-    printArray(num, numSize);
+    sort(array, arraySize);
+    printArray(array, arraySize);
 
     printf("\n\n");
     return EXIT_SUCCESS;
 }
 
-void sort(int array[], int size) {
+void sort(char array[], int size) {
     int temp;
 
     //  bubble sort
-    for (int i = 0; i < size; i++) {        //  ripeto per n elementi dell'array
-        for (int j = 0; j < size; j++) {    //  scorro tutti gli elementi dell'array
-            if (array[j] > array[j + 1]) {  //  controllo se il primo è più grande del secondo
+    for (int i = 0; i < size - 1; i++) {      //  ripeto per n elementi dell'array
+        for (int j = 0; j < size - 1; j++) {  //  scorro tutti gli elementi dell'array
+            if (array[j] > array[j + 1]) {    //  controllo se il primo è più grande del secondo
                 //  swappo il valore x con il valore y
                 temp = array[j];
                 array[j] = array[j + 1];
@@ -38,8 +38,8 @@ void sort(int array[], int size) {
     }
 }
 
-void printArray(int array[], int size) {
+void printArray(char array[], int size) {
     for (int i = 0; i < size; i++) {
-        printf("%d ", array[i]);
+        printf("%c ", array[i]);
     }
 }
